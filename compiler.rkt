4f8a4ff6945eac9b8619ce1e29d28e671942819a
rexim <reximkut@gmail.com>
2018-10-18 02:05:14 +0700

    TODO(#6)

--- compiler.rkt
+++ compiler.rkt
@@ -4,6 +4,6 @@
 (require racket/pretty)
 (require "wacket.rkt")
 
-;;; TODO: compiler only compiles first expresion from stdin
+;;; TODO(#6): compiler only compiles first expresion from stdin
 (pretty-write (wat-module
                (wat-compile (read))))
