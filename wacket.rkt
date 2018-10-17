618605cf704c1242146a4d64fc245e1763a2adb9
rexim <reximkut@gmail.com>
2018-10-18 01:27:15 +0700

    Simplify the compiler

--- wacket.rkt
+++ wacket.rkt
@@ -2,9 +2,7 @@
 
 (provide wat-compile
          wat-module
-         wat-export-func
-         wat-add
-         wat-sub)
+         wat-export-func)
 
 (define (wat-module expr)
   `(module ,expr))
@@ -13,56 +11,24 @@
   `(func (export ,name) (result i32)
          ,@body))
 
-(define (wat-op op xs)
-  (append
-   (for/list ([x xs])
-     `(i32.const ,x))
-   (for/list ([_ (in-range (sub1 (length xs)))])
-     op)))
-
-(define (wat-add xs)
-  (wat-op 'i32.add xs))
-
-(define (wat-sub xs)
-  (wat-op 'i32.sub xs))
-
-(define (wat-mul xs)
-  (wat-op 'i32.mul xs))
-
 (define (wat-number x)
   `(i32.const ,x))
 
+(define (wat-compile-op op xs)
+  (append
+   (append* (for/list ([x xs]) (wat-compile x)))
+   (for/list ([_ (in-range (sub1 (length xs)))])
+     op)))
+
 (define (wat-compile expr)
-  (define (wat-compile-impl expr acc)
-    (match expr
-      [(list (quote +) xs ...)
-       (append
-        (for/list ([_ (in-range (sub1 (length xs)))])
-          'i32.add)
-        (foldl wat-compile-impl
-               acc
-               xs))]
-      [(list (quote -) xs ...)
-       (append
-        (for/list ([_ (in-range (sub1 (length xs)))])
-          'i32.sub)
-        (foldl wat-compile-impl
-               acc
-               xs))]
-      [(list (quote *) xs ...)
-       (append
-        (for/list ([_ (in-range (sub1 (length xs)))])
-          'i32.mul)
-        (foldl wat-compile-impl
-               acc
-               xs))]
-      [(list (quote /) xs ...)
-       (append
-        (for/list ([_ (in-range (sub1 (length xs)))])
-          'i32.div_s)
-        (foldl wat-compile-impl
-               acc
-               xs))]
-      [x #:when (integer? x) (cons (wat-number x) acc)]
-      [unsupported (error "Cannot compile expression:" unsupported)]))
-  (reverse (wat-compile-impl expr null)))
+  (match expr
+    [(list (quote +) xs ...)
+     (wat-compile-op 'i32.add xs)]
+    [(list (quote -) xs ...)
+     (wat-compile-op 'i32.sub xs)]
+    [(list (quote *) xs ...)
+     (wat-compile-op 'i32.mul xs)]
+    [(list (quote /) xs ...)
+     (wat-compile-op 'i32.div_s)]
+    [x #:when (integer? x) (list (wat-number x))]
+    [unsupported (error "Cannot compile expression:" unsupported)]))
